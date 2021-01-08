ad9d14d777da3bcab3ffedf6d9b1f0a7b6ba39ac
Vinicus <viniciusalcantarabarros@gmail.com>
2021-01-08 14:19:54 -0300

    Substituindo o HttpRequest por Fetch

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -82,9 +82,9 @@ class User{
                 let promise;
 
                 if (this.id) {
-                    promise = HttpRequest.put(`/users/${this.id}`, this.toJSON())
+                    promise = Fetch.put(`/users/${this.id}`, this.toJSON())
                 } else {
-                    promise = HttpRequest.post(`/users`, this.toJSON())
+                    promise = Fetch.post(`/users`, this.toJSON())
                 }               
                 promise.then(data => {
                     this.loadFromJSON(data)
@@ -96,7 +96,7 @@ class User{
         }
 
         remove(){
-            return HttpRequest.delete(`/users/${this.id}`)
+            return Fetch.delete(`/users/${this.id}`)
         }
 
 }
